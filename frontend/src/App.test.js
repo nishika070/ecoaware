b449daccd8275a63959222f7a514ec4e293a7f6f
nishika070 <nishikaofficial07@gmail.com>
2026-04-16 23:35:05 +0530

    clean up done

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders ecoaware navigation", () => {
-  render(<App />);
-  const homeElement = screen.getByText(/EcoAware Delhi/i);
-  expect(homeElement).toBeInTheDocument();
-});
